8c65bc9c4ee3a8b3fee4ab965d80c0f32d12afd4
shashanksrajput <shashank12rajput@gmail.com>
2023-02-13 23:48:06 +0530

    adding style to the body

---
+++ index.js
@@ -0,0 +1,18 @@
+// Case 1 (the element is found in the array by taking its position).
+let arr = [2, 3, 8, 7, 9, 6];
+
+      //the array which are present in given index.
+      console.log(arr[3]);
+
+    
+
+// Case 2 (the element didn't find is return as -1) .
+function evenodd(n) {
+   if (n%2==0) {
+
+    return true
+   }
+
+  else return-1; 
+   }  
+console.log(evenodd(3));
